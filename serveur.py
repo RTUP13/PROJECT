403064b00b34dcf0b374524e8d9f2f0b7b44e55d
RTUP13 <48443448+RTUP13@users.noreply.github.com>
2019-03-30 20:31:34 +0100

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -42,25 +42,26 @@ def index():
 @app.route('/<string:page_name>/')
 def render_static(page_name):
     """ La page d'acceuil MYGEM """
-    return render_template( '{page}.html'.format(page=page_name))
-# @app.route('/MYGEM/<string:page_name>/')
+    shelf = get_db()
+    keys = list(shelf.keys())
+    noms = []
+    for key in keys:
+        noms.append(shelf[key]['nom'])
+    return render_template( '{page}.html'.format(page=page_name),list_dyna=noms)
+#@app.route('/MYGEM/<string:page_name>/')
 # def render_static(page_name):
 #     """ Les pages des machines """
 #     return render_template( '{page}.html'.format(page=page_name),)
 
-class ListEquipements(Resource):
-    def get(self):
-        shelf = get_db()
-        keys = list(shelf.keys())
-
-        devices = []
-
-        for key in keys:
-            devices.append(shelf[key])
-        return {'message':'Réussie', 'data': devices }, 200
-
+@app.route('/MYGEM/equipements/', methods=['GET', 'POST']) #les methodes(requetes) accepter sont GET and POST
 # utilisation de la méthode post pour l'enregistrement d'un nouvel équipement dans la bd
-    def post(self):
+def form_equipements():
+    if request.method == 'POST':  #Ce block n'est édité que lorsque le formulaire a été remplie
+        identifiant = request.form.get('identifiant')
+        nom = request.form['nom']
+        type = request.form['type']
+        ip_controleur = request.form['ip_controleur']
+
         parser = reqparse.RequestParser()
 
         parser.add_argument('identifiant', required=True)
@@ -74,7 +75,48 @@ class ListEquipements(Resource):
 
         shelf = get_db()
         shelf[args['identifiant']] = args
-        return {'message':'Equipement enregistré', 'data': args }, 201
+        return '''<h5>L'équipement {} a bien été enregistré</h5>
+                  <h5>Récapitulatif du nouvel équipement</h5>
+                  <h5> identifiant : {} </h5>
+                  <h5> nom : {}</h5>
+                  <h5> type : {} </h5>
+                  <h5> ip_controleur : {}</h5>'''.format(nom,identifiant, nom, type, ip_controleur)
+
+    return '''<form method="POST">
+                  identifiant: <input type="text" name="identifiant"><br>
+                  nom: <input type="text" name="nom"><br>
+                  type: <input type="text" name="type"><br>
+                  ip_controleur : <input type="text" name="ip_controleur"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+
+class ListEquipements(Resource):
+    def get(self):
+        shelf = get_db()
+        keys = list(shelf.keys())
+
+        devices = []
+
+        for key in keys:
+            devices.append(shelf[key])
+        return {'message':'Réussie', 'data': devices }, 200
+
+# utilisation de la méthode post pour l'enregistrement d'un nouvel équipement dans la bd
+    # def post(self):
+    #     parser = reqparse.RequestParser()
+    #
+    #     parser.add_argument('identifiant', required=True)
+    #     parser.add_argument('nom', required=True)
+    #     parser.add_argument('type', required=True)
+    #     parser.add_argument('ip_controleur', required=True)
+    #
+    #
+    #     # Insertion d'un argument dans l'objet
+    #     args = parser.parse_args()
+    #
+    #     shelf = get_db()
+    #     shelf[args['identifiant']] = args
+    #     return {'message':'Equipement enregistré', 'data': args }, 201
 
 class Equipement(Resource):
     def get(self,identifiant):
@@ -96,15 +138,30 @@ class Equipement(Resource):
 
 #@app.route('/equipements/Machine1')
 class ResultSonde(Resource):
-    def get(self,identifiant):
+    def post(self,identifiant):
         data = {"identifiant":identifiant}
-        reponse = requests.get('http://127.0.0.1:8080/equipements/', data = data)
+        reponse = requests.get('http://127.0.0.1:8080/equipements/Machine2')
         reponse = reponse.json()
 
+        identifiant = data
         nom_utilisateur = reponse['data']['nom_utilisateur']
         temperature = reponse['data']['temperature']
         memoire = reponse['data']['TailleDispo']
         temps_connexion = reponse['data']['TempsAllumage']
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('identifiant', required=True)
+        parser.add_argument('nom_utilisateur', required=True)
+        parser.add_argument('temperature', required=True)
+        parser.add_argument('memoire', required=True)
+        parser.add_argument('temps_connexion', required=True)
+
+        # Insertion d'un argument dans l'objet
+        args = parser.parse_args()
+
+        shelf = get_db()
+        shelf[args['identifiant']] = args
+        #return {'message':'Equipement enregistré', 'data': args }, 201
         return render_template('fiche_machine.html', nom_utilisateur=nom_utilisateur, temperature=temperature, memoire=memoire, temps_connexion=temps_connexion)
 
 if __name__ == '__main__':
@@ -113,5 +170,3 @@ if __name__ == '__main__':
 api.add_resource(ListEquipements,'/MYGEM/equipements')
 api.add_resource(Equipement,'/MYGEM/equipements/<string:identifiant>/details')
 api.add_resource(ResultSonde,'/MYGEM/equipements/<string:identifiant>')
-
-#A l'aide d'une template html :
