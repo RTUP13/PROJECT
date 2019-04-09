1b71ad43e8532040e604bb5530356c40e2a6d24b
RTUP13 <48443448+RTUP13@users.noreply.github.com>
2019-04-09 20:43:50 +0200

    Update deploiement_serveur.sh

--- deploiement_serveur.sh
+++ deploiement_serveur.sh
@@ -2,11 +2,13 @@ cd PROJECT
 
 mkdir templates
 mkdir -p static/css
+mkdir -p static/js
 
 mv MYGEM.css fiche_machine.css static/css/
 
-mv MYGEM.html ajout_equipement.html detail_equipement.html fiche_machine.html forme_ajout.html login.html logo.png supprim_equipement.html templates/
+mv MYGEM.html ajout_equipements.html detail_equipement.html fiche_machine.html forme_ajout.html login.html logo.png supprim_equipements.html templates/
 
+mv fonction.js static/js
 python deploiement_client.py
 
 host=`sudo ifconfig | grep 192 | cut -d"t" -f2 | cut -d"n" -f1`
@@ -18,7 +20,7 @@ echo -e "$password\n" | sudo -S apt-get install -y python3-venv
 python3 -m venv venv
 source venv/bin/activate
 
-pip install flask requests
+pip install flask requests, flask_restful markdown
 
 chmod +x -R ~/PROJECT
 export FLASK_APP=serveur.py
